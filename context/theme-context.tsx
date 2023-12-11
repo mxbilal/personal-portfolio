a055bfc58a6bacea6cd7715f7fba172d3649bbd0
bilal <mxbilal0@gmail.com>
2023-12-11 22:09:57 +0500

    fix

--- context/theme-context.tsx
+++ context/theme-context.tsx
@@ -18,7 +18,7 @@ const ThemeContext = createContext<ThemeContextType | null>(null);
 export default function ThemeContextProvider({
   children,
 }: ThemeContextProviderProps) {
-  const [theme, setTheme] = useState<Theme>("light");
+  const [theme, setTheme] = useState<Theme>("dark");
 
   const toggleTheme = () => {
     if (theme === "light") {
@@ -41,7 +41,7 @@ export default function ThemeContextProvider({
       if (localTheme === "dark") {
         document.documentElement.classList.add("dark");
       }
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    } else {
       setTheme("dark");
       document.documentElement.classList.add("dark");
     }
